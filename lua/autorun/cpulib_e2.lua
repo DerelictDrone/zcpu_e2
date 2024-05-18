90d84ddd39338cd4df3e01d39c2c9153e5759acc
DerelictDrone <gage@echelonfit.com>
2024-05-17 22:49:54 -0500

    Start of work on type awareness and table conversion

--- lua/autorun/cpulib_e2.lua
+++ lua/autorun/cpulib_e2.lua
@@ -9,7 +9,25 @@ if myCPUExtension then
 
 	local function addE2Functions(VM,Operands)
 		VM.E2Contexts = {}
-		VM.TargetedE2Context = 0 -- intenional wipe of previous state
+		VM.TargetedE2Context = 0 -- intentional wipe of previous state
+		VM.E2TypeInfo = {TypeNames={},TypeIDs={}} -- Every E2 type with an arbitrary ID, to automatically generate an ENUM for types
+		-- Rather than getting a CRC of the full type name, just pack the shortened type name into a 32 bit number
+		-- None of the current E2 types from all of the cores seem to be over 3 bytes, and any collisions would
+		-- have just wiped a type out before it got into this table, so all of them should be unique.
+		-- Plus, the algorithm should be fairly simple to replicate and reverse for ZCPU users.
+		for k,i in pairs(wire_expression_types2) do
+			local bytes = table.Pack(string.byte(k,1,4));
+			if bytes then
+				local num = bit.lshift(bytes[1] or 0,8)
+				num = bit.lshift(bit.bor(num,bytes[2] or 0),8)
+				num = bit.lshift(bit.bor(num,bytes[3] or 0),8)
+				num = bit.bor(num,bytes[4] or 0)
+				-- Dual lookup so we can get name from ID and ID from name easily
+				VM.E2TypeInfo.TypeNames[k] = {id=num,typeinfo=i}
+				VM.E2TypeInfo.TypeIDs[num] = {name=k,typeinfo=i}
+			end
+		end
+
 		function VM:ReadString(address)
 			local charString = ""
 			local charCount = 0
@@ -156,6 +174,7 @@ if myCPUExtension then
 	local function ExecuteE2(VM,Operands)
 		if checkContextValid(VM,Operands[2]) then
 			local success,msg = VM.E2Contexts[Operands[2]].E2Func(VM.E2Contexts[Operands[2]].context)
+			PrintTable(VM.E2Contexts[Operands[2]])
 			if not success then
 				VM.E2Contexts[Operands[2]].StatusMsg = msg
 				Operands[1] = 1
@@ -174,7 +193,7 @@ if myCPUExtension then
 		{"W1"},
 		{
 			Version = 0.42,
-			Description = "Runs E2 handle from Register 2 and 0 writes to Register 1 on success or writes 1 if an error occured in execution"
+			Description = "Runs E2 handle from Register 2 and writes 0 to Register 1 on success or writes 1 if an error occured in execution"
 		}
 	)
 
@@ -218,7 +237,11 @@ if myCPUExtension then
 		if checkContextValid(VM,VM.TargetedE2Context) then
 			local str = VM:ReadString(Operands[2])
 			if str then
-				Operands[1] = VM.E2Contexts[VM.TargetedE2Context].context.GlobalScope[str]
+				if isnumber(VM.E2Contexts[VM.TargetedE2Context].context.GlobalScope[str]) then
+					Operands[1] =  VM.E2Contexts[VM.TargetedE2Context].context.GlobalScope[str]
+				else
+					Operands[1] = 0
+				end
 			end
 		end
 	end
@@ -251,5 +274,81 @@ if myCPUExtension then
 			Description = "Writes CPU Entity to variable using Register 1 as Variable Name"
 		}
 	)
-
+	-- size lookup for lua / gmod types that have a type that isn't just a table internally
+	local primitiveSizeLookup = {
+		n = 1, -- number / normal
+		xv2 = 2, -- VECTOR2
+		c = 2, -- COMPLEX
+		v = 3, -- vector3
+		a = 3, -- angle
+		q = 4, -- QUATERNION
+		xv4 = 4, -- VECTOR4
+		xm2 = 4, -- MATRIX2
+		m = 9, -- MATRIX
+		xm4 = 16, -- MATRIX4
+	}
+	local function identifyType(var)
+		local t = type(var) -- Check if it's a primitive type or not
+		if var == "table" then -- var is a table, will need probing to discover its true type
+			if var.ntypes or var.stypes then
+				return "t" -- var is a typed E2 table, it has a list of type indices
+			end
+			return "r" -- Type unknown, possibly an array because it had no identifiable metadata
+		end
+	end
+	local function sumTypeArray(VM,arr)
+		local sum = 0
+		for ind,i in ipairs(arr) do
+			local primitive = primitiveSizeLookup[i]
+			if primitive then
+				sum = sum + primitive
+			else
+				local E2TypeDefault = VM.E2TypeInfo.TypeNames[i][2]
+				if type(E2TypeDefault) == "table" then
+					sum = sum + #E2TypeDefault -- If type has a default value to initialize to, uses that default value's size
+				end
+			end
+		end
+	end
+	local function sumTypeDict(VM,dict)
+		-- just convert the typedict to a typearray and use sumTypeArray after we sum the size of the keys
+		local typearr = {}
+		local stringSizes = 0
+		for k,v in pairs(dict) do
+			stringSizes = #k + 1 -- + 1 because it needs a null terminator after each
+			table.insert(typearr,v)
+		end
+		return stringSizes,sumTypeArray(typearr)
+	end
+	local function getE2TableDataSize(VM,table)
+		local metadatasize = 7 -- metadata amount for storing the table's offsets, unchanging
+		local numindex_size = (#table.ntypes)*2 -- size required for storing the numeric indexes, without their values
+		local numindex_value_size = sumTypeArray(VM,table.ntypes)
+		local strindex_size = (#table.stypes)*2
+		local str_name_size, strindex_value_size = sumTypeDict(VM,table.stypes)
+	end
+	-- Returns the size of a variable type, or -1 if it cannot be converted
+	local function readVariableSize(VM,Operands)
+		if checkContextValid(VM,VM.TargetedE2Context) then
+			local str = VM:ReadString(Operands[1])
+			if str then
+				local var = VM.E2Contexts[VM.TargetedE2Context].context.GlobalScope[str]
+				local e2type = identifyType(var)
+				if e2type == "t" then
+					getE2TableDataSize(VM,var)
+				end
+			end
+		end
+	end
+	
+	myCPUExtension:InstructionFromLuaFunc(
+		"E2_GET_SIZE",
+		2,
+		readVariableSize,
+		{"W1"},
+		{
+			Version = 0.42,
+			Description = "Get size of variable [UNFINISHED]"
+		}
+	)
 end
